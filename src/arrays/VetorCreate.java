57c1a757a75ee49dd08daa697a61bcaaa0750860
Francis <irenofrancinato@gmail.com>
2025-10-20 15:55:08 -0300

    Revisando conceitos básicos de arrays

---
+++ src/arrays/VetorCreate.java
@@ -0,0 +1,14 @@
+package arrays;
+
+public class VetorCreate {
+
+    private String[] elementos;
+    private int tamanho;
+
+    public VetorCreate(final int tamanho) {
+        this.elementos = new String[tamanho];
+        this.tamanho = 0;
+    }
+
+
+}
